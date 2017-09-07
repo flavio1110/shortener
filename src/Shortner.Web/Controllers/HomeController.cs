99ca6974a27aa50fbf6a54066d23867379fca008
Flávio Silva <flavio1110@gmail.com>
2017-09-07 21:37:58 +0200

    initial commit

---
+++ src/Shortner.Web/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shortner.Web.Models;
+
+namespace Shortner.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UrlShortener model)
+        {
+            model = model ?? new UrlShortener();
+            model.ShortUrl = "abcd";
+            return PartialView("_UrlShortener", model);
+        }
+        
+    }
+}
---
+++ src/Shortner.Web/Models/UrlShortener.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shortner.Web.Models
+{
+    public class UrlShortener
+    {
+        public string RawUrl { get; set; }
+
+        public string ShortUrl { get; set; }
+    }
+}
