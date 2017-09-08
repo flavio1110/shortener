a3db90984706abc5d94d80d47c487615d5e02ee3
Flávio Silva <flavio1110@gmail.com>
2017-09-08 21:47:09 +0200

    mysql connection working

--- src/Shortner.Web/Controllers/HomeController.cs
+++ src/Shortner.Web/Controllers/HomeController.cs
@@ -1,25 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Shortner.Web.Data;
 using Shortner.Web.Models;
 
 namespace Shortner.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DbUrlContext dbUrlContext;
+
+        public HomeController(DbUrlContext dbUrlContext)
+        {
+            this.dbUrlContext = dbUrlContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(UrlShortener model)
+        public async Task<IActionResult> Index(UrlShortener model)
         {
             model = model ?? new UrlShortener();
             model.ShortUrl = "abcd";
+
+            await dbUrlContext.AddAsync(new LongUrl
+            {
+                Url = model.RawUrl,
+                Date = DateTime.UtcNow
+            });
+            await dbUrlContext.SaveChangesAsync();
+
             return PartialView("_UrlShortener", model);
         }
         
--- src/Shortner.Web/Startup.cs
+++ src/Shortner.Web/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shortner.Web.Data;
 
 namespace Shortner.Web
 {
@@ -18,13 +20,19 @@ namespace Shortner.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var sqlConnectionString = Configuration.GetConnectionString("default");
+            services.AddDbContext<DbUrlContext>(options =>
+                options.UseMySql(
+                    sqlConnectionString,
+                    b => b.MigrationsAssembly("Shortner.Web")
+                )
+            );
+
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
